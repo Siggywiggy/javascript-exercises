87cee960ec8fb0805bf24c3b2861fdde42751503
Siggywiggy <kurmaadam@gmail.com>
2025-02-16 19:55:48 +0200

    excercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let maxIndex = string.length - 1;
+    let result = "";
+    for (maxIndex; maxIndex >= 0; --maxIndex){
+        console.log(string.charAt(maxIndex));
+        result += string.charAt(maxIndex);
+    }
+    return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, extras) {
+    console.log(arguments)
+   
 };
 
+console.log("arguments original")
+removeFromArray([1,2,3], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
