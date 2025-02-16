4748582a88f2415bf5a1255bfc0c6f9c7a78a8d0
Siggywiggy <kurmaadam@gmail.com>
2025-02-16 23:01:19 +0200

    excercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,37 @@
-const removeFromArray = function(inputArray, extras) {
-    console.log(arguments)
-   
-};
+const removeFromArray = function(inputArray, ...extras) {
+    let result = ""
 
-console.log("arguments original")
-removeFromArray([1,2,3], 3)
+    if (inputArray === null || inputArray === undefined) {
+        return undefined;
+    } else if (extras.length === 0)  {
+        return inputArray;
+    }
+
+    arrLength = inputArray.length
+
+    for (let i = 0; i < arrLength; i++) {
+        let match = false;
+
+        for (const arg of extras) {
+            console.log(`${inputArray[i]} - ${arg}`)
+            if (inputArray[i] === arg) {
+                let match = true;
+                console.log("match found!")
+
+            } 
+
+        if (match === false) {
+            console.log(`adding ${inputArray[i]}`)
+            result += inputArray[i] 
+            }
+        }
+    }
+
+    return result;
+
+}
+
+
+console.log(removeFromArray([1,2,3], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
