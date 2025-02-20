4b2bec64791a93fcd71faeba9c31b024d2e29675
Siggywiggy <kurmaadam@gmail.com>
2025-02-20 11:31:20 +0200

    Fibonacci func passes tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    // check if input is string and if so parse it as a full integer
+    if (Number(count) === NaN) {
+        return "OOPS";
+    } else {
+        count = parseInt(count);
+    }
+    // check if the nth asked fib number is negative or 0
+    if (count < 0) {
+        return "OOPS";
+    } else if (count === 0) {
+        return 0;
+    }
 
+    // declare first two fibonacci numbers: 1 and 1
+
+    let first = 1;
+    let second = 1;
+
+    // because first 2 fib numbers are already declared, our loop must
+    // run to count -2
+    for (let i = 1; i < count; i++) {
+        [second, first] = [second + first, second]; // array destructuring
+    }
+    return first;
 };
+
+console.log(fibonacci(6))
 
 // Do not edit below this line
 module.exports = fibonacci;
